5cd0c73947ae5f14321f7cf6c436a9716d35b66f
Rohith <gambol99@gmail.com>
2014-11-27 10:26:43 +0000

    adding the embassy proxy

--- dockers/embassy/stage/startup.sh
+++ dockers/embassy/stage/startup.sh
@@ -6,12 +6,31 @@
 #  vim:ts=2:sw=2:et
 #
 
+DISCOVERY=${DISCOVERY:-"etcd://localhost:4001"}
 INTERFACE=${PROXY_INTERFACE:-eth0}
-VERBOSE=${VERBOSE:-0}
+VERBOSITY=${VERBOSITY:-0}
+PROXY_PORT="9999"
+RULE_COMMENT="embassy_redirection"
+RULE_CHAIN="PREROUTING"
+IPTABLES="/sbin/iptables"
 
-if [ -z "$DISCOVERY" ]; then
-  echo "You have not specified the discovery uri, -e DISCOVERY=[BACKEND]"
-  exit 1
-fi
+failed() {
+  [ -n "$@" ] || {
+    echo "Error: $@";
+    exit 1
+  }
+}
 
-/bin/embassy -discovery ${DISCOVERY} -interface ${INTERFACE} -alsologtostderr=true -v ${VERBOSE}
+ADD_REDIRECT_RULE="-t nat -A PREROUTING -p tcp -j REDIRECT --to-ports ${PROXY_PORT}"
+
+iptables_setup() {
+  # step: check if the rule is already there
+  $IPTABLES $ADD_REDIRECT_RULE || failed "unable to add the iptables redirection rule"
+}
+
+echo "Adding the iptables redirection rule: $ADD_REDIRECT_RULE"
+iptables_setup
+echo "Iptables Prerouting Table"
+$IPTABLES -t nat -L PREROUTING --line-numbers
+echo "Starting the Embassy Proxy Service"
+/bin/embassy -discovery ${DISCOVERY} -interface ${INTERFACE} -logtostderr=true -v ${VERBOSITY}
