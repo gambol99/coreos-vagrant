baec3248c96ef9104321c2c31d7d74ed92e09d8f
Rohith <gambol99@gmail.com>
2015-01-15 16:01:49 +0000

    [dockers] - fixing up the path of the config fike

--- dockers/elasticsearch/config/startup.sh
+++ dockers/elasticsearch/config/startup.sh
@@ -1,7 +1,20 @@
+#!/bin/bash
 #
 #   Author: Rohith (gambol99@gmail.com)
 #   Date: 2015-01-15 15:24:24 +0000 (Thu, 15 Jan 2015)
 #
 #  vim:ts=2:sw=2:et
 #
+VERSION="1.4.1"
+NAME="Elasticsearch ${VERSION}"
+SERVICE_DELAY=${SERVICE_DELAY:-5}
 
+annonce() {
+  echo "* $@"
+}
+
+annonce "Waiting for ${SERVICE_DELAY} seconds before starting up ${NAME}"
+sleep ${SERVICE_DELAY}
+
+annonce "Starting ${NAME} service"
+/usr/share/elasticsearch/bin/elasticsearch
